3e0fd936d7ba2593565665ab40d44387bfaf39bc
adamchappell00 <adam.chappell00@gmail.com>
2022-02-22 12:20:30 -0600

    added landing page

---
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -0,0 +1,25 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+    @GetMapping("/")
+    @ResponseBody
+    public String index(){
+        return "This is the landing page!";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String hello(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String increment(@PathVariable int number){
+        return number + " plus one is " + (number + 1) + "!";
+    }
+}
