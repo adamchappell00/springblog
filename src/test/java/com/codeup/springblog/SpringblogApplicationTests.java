b285eb6dc74ce6e425bc5de7f14415ca1b467e24
adamchappell00 <adam.chappell00@gmail.com>
2022-03-01 15:54:53 -0600

    Testing dependencies and initial @Before setup test method added

--- src/test/java/com/codeup/springblog/SpringblogApplicationTests.java
+++ src/test/java/com/codeup/springblog/SpringblogApplicationTests.java
@@ -1,13 +1,73 @@
 package com.codeup.springblog;
 
+import com.codeup.springblog.models.User;
+import com.codeup.springblog.repositories.PostRepository;
+import com.codeup.springblog.repositories.UserRepository;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+
+import javax.servlet.http.HttpSession;
+
+import static org.springframework.http.RequestEntity.post;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest(classes = SpringblogApplication.class)
 class SpringblogApplicationTests {
+    private User testUser;
+    private HttpSession httpSession;
 
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    UserRepository userDao;
+    @Autowired
+    PostRepository postDao;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Before
+    public void setup() throws Exception {
+        testUser = userDao.findByUsername("testUser");
+
+        // Creates the test user if not exists
+        if(testUser == null){
+            User newUser = new User();
+            newUser.setUsername("testUser");
+            newUser.setPassword(passwordEncoder.encode("password"));
+            newUser.setEmail("testUser@codeup.com");
+            testUser = userDao.save(newUser);
+        }
+        httpSession = this.mvc.perform(post("/login").with(csrf())
+                        .param("username", "testUser")
+                        .param("password", "password"))
+                .andExpect(status().is(HttpStatus.FOUND.value()))
+                .andExpect(redirectedUrl("/posts"))
+                .andReturn()
+                .getRequest()
+                .getSession();
+
+    }
+
+
+
+    // Further Tests Below
     @Test
     void contextLoads() {
     }
+
+
 
 }
