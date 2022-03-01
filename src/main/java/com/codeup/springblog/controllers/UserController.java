ebf587ca3b4fd7632280658296abf4ce8d14cb90
adamchappell00 <adam.chappell00@gmail.com>
2022-03-01 12:14:33 -0600

    UserController added with UserDao and Password encoder, register and login mappings

---
+++ src/main/java/com/codeup/springblog/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.codeup.springblog.controllers;
+
+import com.codeup.springblog.models.User;
+import com.codeup.springblog.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+public class UserController {
+    private UserRepository userDao;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+        model.addAttribute("user", new User());
+        return "/register";
+    }
+
+    @PostMapping("/register")
+    public String submitRegistrationForm(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
+    }
+}
+
+
+}
